9423a21cb7ca989510544ee27a15e19276f586ab
root <jason@pushshift.io>
2015-12-12 10:10:41 +0000

    Fixed rsapp getComments function

--- docker-containers/app/rsapp/lib/Common.pm
+++ docker-containers/app/rsapp/lib/Common.pm
@@ -3,7 +3,7 @@ my $dbh = shift;
 my $comment_ids = shift;
 my @comments;
 return unless scalar @$comment_ids > 0;
-my $query = "SELECT comment_id, body from comment WHERE comment_id IN (" . join(",",@$comment_ids) . ")";
+my $query = "SELECT comment_id, json from com_json WHERE comment_id IN (" . join(",",@$comment_ids) . ")";
 my $select = $dbh->prepare($query);
 $select->execute();
 return $select->fetchall_hashref('comment_id');
