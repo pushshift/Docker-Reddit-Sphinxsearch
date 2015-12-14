31816f0684a8388df80e468807cb62ba72d8a371
root <root@instance-1.c.pushshift.internal>
2015-12-14 05:40:06 +0000

    Enhanced loading

---
+++ docker-containers/database/reddit.sql
@@ -0,0 +1,381 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `reddit`
+--
+CREATE DATABASE IF NOT EXISTS `reddit` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `reddit`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE IF NOT EXISTS `author` (
+`author_id` int(10) unsigned NOT NULL,
+  `name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `com_index`
+--
+
+CREATE TABLE IF NOT EXISTS `com_index` (
+  `comment_id` bigint(20) unsigned NOT NULL,
+  `created_utc` int(10) unsigned NOT NULL,
+  `subreddit_id` int(10) unsigned NOT NULL,
+  `link_id` int(10) unsigned NOT NULL,
+  `author_id` int(10) unsigned NOT NULL,
+  `score` mediumint(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Reddit comment index table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `com_json`
+--
+
+CREATE TABLE IF NOT EXISTS `com_json` (
+  `comment_id` bigint(20) unsigned NOT NULL COMMENT 'Comment id',
+  `json` mediumtext CHARACTER SET utf8mb4 NOT NULL COMMENT 'Comment json'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Reddit comment bodies ';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `link_count`
+--
+
+CREATE TABLE IF NOT EXISTS `link_count` (
+  `link_id` bigint(20) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `link_index`
+--
+
+CREATE TABLE IF NOT EXISTS `link_index` (
+  `link_id` int(10) unsigned NOT NULL,
+  `created_utc` int(10) unsigned NOT NULL,
+  `subreddit_id` int(10) unsigned NOT NULL,
+  `author_id` int(10) unsigned NOT NULL,
+  `score` mediumint(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Reddit comment index table';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `link_json`
+--
+
+CREATE TABLE IF NOT EXISTS `link_json` (
+  `link_id` bigint(20) unsigned NOT NULL COMMENT 'Comment id',
+  `json` mediumtext CHARACTER SET utf8mb4 NOT NULL COMMENT 'Comment json'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Reddit comment bodies ';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE IF NOT EXISTS `log` (
+`log_id` int(10) unsigned NOT NULL,
+  `created_utc` int(10) unsigned NOT NULL,
+  `source` varchar(32) NOT NULL,
+  `severity` enum('debug','info','notice','warning','error','critical','alert','emergency') NOT NULL DEFAULT 'info',
+  `message` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subreddit`
+--
+
+CREATE TABLE IF NOT EXISTS `subreddit` (
+  `subreddit_id` int(10) unsigned NOT NULL COMMENT 'Subreddit id',
+  `name` varchar(32) NOT NULL COMMENT 'Subreddit name'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Reddit subreddits';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_day`
+--
+
+CREATE TABLE IF NOT EXISTS `time_day` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `comment_count` int(10) unsigned NOT NULL,
+  `link_count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_hour`
+--
+
+CREATE TABLE IF NOT EXISTS `time_hour` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `comment_count` mediumint(5) unsigned NOT NULL,
+  `link_count` mediumint(5) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_link_hour`
+--
+
+CREATE TABLE IF NOT EXISTS `time_link_hour` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `link_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_link_minute`
+--
+
+CREATE TABLE IF NOT EXISTS `time_link_minute` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `link_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_link_second`
+--
+
+CREATE TABLE IF NOT EXISTS `time_link_second` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `link_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_minute`
+--
+
+CREATE TABLE IF NOT EXISTS `time_minute` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `comment_count` smallint(5) unsigned NOT NULL,
+  `link_count` smallint(5) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_second`
+--
+
+CREATE TABLE IF NOT EXISTS `time_second` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `comment_count` smallint(5) unsigned NOT NULL,
+  `link_count` smallint(5) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_subreddit_day`
+--
+
+CREATE TABLE IF NOT EXISTS `time_subreddit_day` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `subreddit_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_subreddit_hour`
+--
+
+CREATE TABLE IF NOT EXISTS `time_subreddit_hour` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `subreddit_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_subreddit_minute`
+--
+
+CREATE TABLE IF NOT EXISTS `time_subreddit_minute` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `subreddit_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_subreddit_second`
+--
+
+CREATE TABLE IF NOT EXISTS `time_subreddit_second` (
+  `created_utc` int(10) unsigned NOT NULL,
+  `subreddit_id` int(10) unsigned NOT NULL,
+  `count` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+ ADD PRIMARY KEY (`author_id`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `com_index`
+--
+ALTER TABLE `com_index`
+ ADD PRIMARY KEY (`comment_id`), ADD KEY `score` (`score`), ADD KEY `link_id` (`link_id`), ADD KEY `subreddit_id` (`subreddit_id`), ADD KEY `created_utc` (`created_utc`), ADD KEY `author_id` (`author_id`);
+
+--
+-- Indexes for table `com_json`
+--
+ALTER TABLE `com_json`
+ ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `link_count`
+--
+ALTER TABLE `link_count`
+ ADD PRIMARY KEY (`link_id`);
+
+--
+-- Indexes for table `link_index`
+--
+ALTER TABLE `link_index`
+ ADD PRIMARY KEY (`link_id`), ADD KEY `score` (`score`), ADD KEY `subreddit_id` (`subreddit_id`), ADD KEY `created_utc` (`created_utc`), ADD KEY `author_id` (`author_id`);
+
+--
+-- Indexes for table `link_json`
+--
+ALTER TABLE `link_json`
+ ADD PRIMARY KEY (`link_id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+ ADD PRIMARY KEY (`log_id`), ADD KEY `created_utc` (`created_utc`), ADD KEY `severity` (`severity`), ADD KEY `source` (`source`);
+
+--
+-- Indexes for table `subreddit`
+--
+ALTER TABLE `subreddit`
+ ADD PRIMARY KEY (`subreddit_id`), ADD KEY `subreddit-name` (`name`);
+
+--
+-- Indexes for table `time_day`
+--
+ALTER TABLE `time_day`
+ ADD PRIMARY KEY (`created_utc`);
+
+--
+-- Indexes for table `time_hour`
+--
+ALTER TABLE `time_hour`
+ ADD PRIMARY KEY (`created_utc`);
+
+--
+-- Indexes for table `time_link_hour`
+--
+ALTER TABLE `time_link_hour`
+ ADD PRIMARY KEY (`created_utc`,`link_id`), ADD KEY `link_id` (`link_id`);
+
+--
+-- Indexes for table `time_link_minute`
+--
+ALTER TABLE `time_link_minute`
+ ADD PRIMARY KEY (`created_utc`,`link_id`), ADD KEY `link_id` (`link_id`);
+
+--
+-- Indexes for table `time_link_second`
+--
+ALTER TABLE `time_link_second`
+ ADD PRIMARY KEY (`created_utc`,`link_id`), ADD KEY `link_id` (`link_id`);
+
+--
+-- Indexes for table `time_minute`
+--
+ALTER TABLE `time_minute`
+ ADD PRIMARY KEY (`created_utc`);
+
+--
+-- Indexes for table `time_second`
+--
+ALTER TABLE `time_second`
+ ADD PRIMARY KEY (`created_utc`);
+
+--
+-- Indexes for table `time_subreddit_day`
+--
+ALTER TABLE `time_subreddit_day`
+ ADD PRIMARY KEY (`created_utc`,`subreddit_id`), ADD KEY `subreddit_created_utc` (`subreddit_id`,`created_utc`);
+
+--
+-- Indexes for table `time_subreddit_hour`
+--
+ALTER TABLE `time_subreddit_hour`
+ ADD PRIMARY KEY (`created_utc`,`subreddit_id`), ADD KEY `subreddit_created_utc` (`subreddit_id`,`created_utc`);
+
+--
+-- Indexes for table `time_subreddit_minute`
+--
+ALTER TABLE `time_subreddit_minute`
+ ADD PRIMARY KEY (`created_utc`,`subreddit_id`), ADD KEY `subreddit_created_utc` (`subreddit_id`,`created_utc`);
+
+--
+-- Indexes for table `time_subreddit_second`
+--
+ALTER TABLE `time_subreddit_second`
+ ADD PRIMARY KEY (`created_utc`,`subreddit_id`), ADD KEY `subreddit_created_utc` (`subreddit_id`,`created_utc`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+MODIFY `author_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+MODIFY `log_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
--- scripts/load-data.pl
+++ scripts/load-data.pl
@@ -3,7 +3,8 @@
 use strict;
 use warnings;
 use Cpanel::JSON::XS;
-use List::MoreUtils qw /uniq/;
+use Term::ANSIColor qw(:constants);
+use Data::Dumper;
 use DBI;
 use POSIX;
 $|=1;
@@ -20,7 +21,6 @@ my $dbh = DBI->connect(@connection) or die;
 my $insert_json = $dbh->prepare('INSERT IGNORE INTO com_json VALUES (?,?)') or die "Couldn't prepare statement: " . $dbh->errstr;
 my $insert_subreddit = $dbh->prepare('INSERT IGNORE INTO subreddit VALUES (?,?)') or die "Couldn't prepare statement: " . $dbh->errstr;
 my $insert_comindex = $dbh->prepare('INSERT IGNORE INTO `com_index` VALUES (?,?,?,?,?)') or die "Couldn't prepare statement: " . $dbh->errstr;
-
 my $count = 0;
 my %subreddit;
 my %comments;
@@ -29,10 +29,12 @@ while (<STDIN>) {
 chomp $_;
 my $json = decode_json($_);
 my $id = strtol($json->{id},36);
+$json->{original} = $_;
 $comments{$id} = $json;
 unless (++$count % 1000) { 
     removeCommentsAlreadyIndexed(\%comments);
     processRemainingComments(\%comments);
+    print GREEN, "Processed ", commify($count), "\r";
     } 
 }
 
@@ -50,39 +52,87 @@ sub processRemainingComments {
 my $comments = shift;
 my (@sphinx,@com_index,@com_json);
 
-my @authors =  uniq(map { $comments->{$_}->{author} } keys %$comments);
-print scalar @authors;
-die;
+# To avoid the MySQL Auto-increment bug on insert ignore, we have to check records ourselves (This can probably be optimized) 
+my %authors_from_batch = map { $comments->{$_}->{author} => 1 } keys %$comments;
+my %authors_to_insert = %authors_from_batch;
+my $query = qq|SELECT name FROM author WHERE name IN ('| . join("','", map { $_ } keys %authors_from_batch) .  qq|')|;
+my $sth = $dbh->prepare($query);
+$sth->execute;
+my @authors_in_db = map @$_, @{$sth->fetchall_arrayref()};
+delete @authors_to_insert{@authors_in_db};
+bulkInsert($dbh,[map {[$_]} keys %authors_to_insert], "INSERT IGNORE INTO author (name) VALUES ") if keys %authors_to_insert;
+$dbh->commit;
+$sth = $dbh->prepare(qq|SELECT author_id, name FROM author WHERE name IN ('| . join("','", map { $_ } keys %authors_from_batch) .  qq|')|);
+$sth->execute;
+my $author = $sth->fetchall_hashref('name');
+# ----------------------------------------------
+my ($seconds,$minutes,$hours,$days);
+my %indexes;
 
 for (keys %$comments) {
     my $json = $comments->{$_};
-    my $id = $json->{id}; 
+    my $json_encoded = $json->{original};;
+    my $id = strtol($json->{id},36);  
     my $subreddit = $json->{subreddit};
     my $subreddit_id = strtol(substr($json->{subreddit_id},3),36);
     my $link_id = strtol(substr($json->{link_id},3),36);
     my $created_utc = $json->{created_utc};
     my $score = $json->{score};
     my $body = $json->{body};
+    my $author_id = $author->{$json->{author}}->{author_id};
     unless ($subreddit{$subreddit}) {
         $insert_subreddit->execute($subreddit_id,$subreddit);
         $subreddit{$subreddit} = $subreddit_id;
         }
+    $indexes{time_second}{$created_utc}++;
+    $indexes{timer_minute}{floor($created_utc/60)*60}++;
+    $indexes{time_hour}{floor($created_utc/3600)*3600}++;
+    $indexes{time_day}{floor($created_utc/86400)*86400}++;
+    $indexes{time_link_hour}{$link_id}{floor($created_utc/3600)*3600}++;
+    $indexes{time_link_minute}{$link_id}{floor($created_utc/60)*60}++;
+    $indexes{time_link_second}{$link_id}{$created_utc}++;
     push(@sphinx, [$id,$body,$created_utc,$subreddit_id,$link_id,$score]);
-    push(@com_index, [$id,$created_utc,$subreddit_id,$link_id,$score]);
-    push(@com_json, [$id,$_]);
+    push(@com_index, [$id,$created_utc,$subreddit_id,$link_id,$author_id,$score]);
+    push(@com_json, [$id,$json_encoded]);
 }
 
-print ".";
-bulkInsert($dbh,\@com_index, "INSERT IGNORE INTO com_index VALUES ");
-bulkInsert($dbh,\@com_json, "INSERT IGNORE INTO com_json VALUES ");
-bulkInsert($sphinx,\@sphinx, "REPLACE INTO rt (id,body,date,subreddit_id,submission_id,score) VALUES ");
+bulkInsert($dbh,\@com_index, "INSERT IGNORE INTO com_index VALUES ") if @com_index;
+bulkInsert($dbh,\@com_json, "INSERT IGNORE INTO com_json VALUES ") if @com_json;
+bulkInsert($sphinx,\@sphinx, "REPLACE INTO rt (id,body,date,subreddit_id,submission_id,score) VALUES ") if @sphinx;
+
+
+for (qw|time_second time_minute time_hour time_day|) {
+if ($indexes{$_}) {
+my @array;
+for my $s (keys $indexes{$_}) {
+        push(@array,[$s,$indexes{$_}{$s}]);
+}
+bulkInsert($dbh, \@array,"INSERT INTO $_ (created_utc,comment_count) VALUES ", "ON DUPLICATE KEY UPDATE comment_count=comment_count+VALUES(comment_count)");
+}
+}
+
+for (qw|time_link_second time_link_minute time_link_hour|) {
+my @array;
+if ($indexes{$_}) {
+for my $s (keys $indexes{$_}) {
+        for my $t (keys $indexes{$_}{$s}) {
+        push(@array,[$t,$s,$indexes{$_}{$s}{$t}]);
+        }
+}
+bulkInsert($dbh, \@array,"INSERT INTO $_ (created_utc,link_id,count) VALUES ", "ON DUPLICATE KEY UPDATE count=count+VALUES(count)");
+}
+}
+
+
 $dbh->commit;
+undef %$comments;
 }
 
 sub bulkInsert {
 my $db_handle = shift;
 my $records = shift;
 my $query = shift;
+my $end_query = shift || "";
 my $start = 0;
 for (@$records) {
     $query .= ',' if $start++;
@@ -90,7 +140,17 @@ for (@$records) {
        . (join (",", map { $db_handle->quote($_)} @$_))
        .')';
 }
-my $affected = $db_handle->do($query) or die;
+my $affected = $db_handle->do("$query $end_query") or die;
 undef @$records;
+}
+
+sub commify {
+    my ( $sign, $int, $frac ) = ( $_[0] =~ /^([+-]?)(\d*)(.*)/ );
+    my $commified = (
+        scalar reverse join ',',
+        unpack '(A3)*',
+        scalar reverse $int
+    );
+    return $sign . $commified . $frac;
 }
 
