85c20f27dde8a8773afe33184956aab5495e697c
root <jason@redditanalytics.com>
2015-12-15 09:44:10 +0000

    Major DB handling revamp

--- docker-containers/app/rsapp/lib/RSApp.pm
+++ docker-containers/app/rsapp/lib/RSApp.pm
@@ -1,5 +1,7 @@
 package RSApp;
 use Mojo::Base 'Mojolicious';
+use Mojo::mysql;
+#use Mojolicious::Plugin::Database;
 
 # This method will run once at server start
 sub startup {
@@ -8,13 +10,34 @@ sub startup {
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer');
 
+  # Database Connection
+
+  $self->helper(db => sub { state $pg = Mojo::mysql->new('mysql://root@rs-database/reddit') });
+  $self->helper(sphinx => sub { state $sphinx = Mojo::mysql->new('mysql://root@rs-sphinxsearch:9306/rt') });
+#  $self->plugin('database', {
+#            databases => {
+#                'db' => {
+#                 dsn      => 'dbi:mysql:dbname=reddit;host=rs-database',
+#                 username => 'root',
+#                 password => '',
+#                 options  => { AutoCommit => 1 },
+#                 },
+#                'sphinx' => {
+#                    dsn      => 'dbi:mysql:database=rt;host=rs-sphinxsearch;port=9306',
+#                },
+#            },
+#        });
+
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
   $r->get('/')->to('example#welcome');
   #$r->get('/reddit/:action')->to('reddit#');
-  $r->get('/reddit/search')->to('reddit#search');
+  #$r->get('/reddit/search/')->to('reddit#search');
+  $r->get('/reddit/search/comments')->to(controller => 'reddit', action => 'searchComments');
+  $r->get('/reddit/search/author')->to(controller => 'reddit', action => 'searchAuthor');
+  $r->get('/reddit/search/')->to(controller => 'reddit', action => 'search', type => undef);
   $r->get('/reddit/activity')->to('redditactivity#activity');
   $r->get('/reddit/activity/:time')->to('redditactivity#activity');
   $r->get('/status/tables')->to('main#tablestatus');
--- docker-containers/app/rsapp/lib/RSApp/Controller/Reddit.pm
+++ docker-containers/app/rsapp/lib/RSApp/Controller/Reddit.pm
@@ -9,72 +9,59 @@ use HTML::Entities;
 use Cpanel::JSON::XS;
 use Time::HiRes 'time';
 use DBI;
+$|=1;
 
-# This action will render a template
-
-sub search {
+sub searchAuthor {
 my $self = shift;
-my @connection = ("DBI:mysql:reddit;host=rs-database", "root", "", {
-                PrintError => 1,
-                AutoCommit => 1,
-                mysql_enable_utf8 => 1,
-                mysql_write_timeout => 10,
-                RaiseError => 1,
-                });
-
-my $sphinx = DBI->connect("DBI:mysql:database=rt;host=rs-sphinxsearch;port=9306") or die $!;
-my $dbh = DBI->connect(@connection) or die;
-my $data;
-$data->{data} = undef;
-my $limit = $self->param('limit') ? int $self->param('limit') : 25;
-if ($limit > 100) {$limit = 100}
-my $query = $sphinx->prepare(qq|
-SELECT id, count(*) c, submission_id, subreddit_id, score FROM rt 
+my $dbh = getDBHandle();
+my $sphinx = getSphinxHandle();
+my $query = $dbh->prepare(qq|
+SELECT id, count(*) c, submission_id, subreddit_id, score FROM rt
 WHERE match(?) GROUP BY submission_id
 WITHIN GROUP ORDER BY score DESC
 ORDER BY c DESC
 LIMIT ?
 |);
+}
+
+sub searchComments {
+my $self = shift;
+my $limit = $self->param('limit') ? int $self->param('limit') : 25;
+if ($limit > 100) {$limit = 100}
+my $results = $self->sphinx->db->query("SELECT * FROM rt WHERE match(?) ORDER BY date DESC LIMIT ?",$self->param('q'),$limit)->hashes;
+$self->render(json => $results);
+}
+
+
+sub search {
+my $self = shift;
+my $data;
+$data->{data} = undef;
+my $limit = $self->param('limit') ? int $self->param('limit') : 25;
+if ($limit > 100) {$limit = 100}
+my $query = qq|
+SELECT id, count(*) c, submission_id, subreddit_id, score FROM rt 
+WHERE match(?) GROUP BY submission_id
+WITHIN GROUP ORDER BY score DESC
+ORDER BY c DESC
+LIMIT ?|;
+
 my $time = time;
-$query->execute($self->param('q'),$limit);
+my $results = $self->sphinx->db->query($query,$self->param('q'),$limit)->hashes;
 $data->{debug}->{sphinxtime} = time - $time;
-my $arr_ref = $query->fetchall_arrayref({});
 
 # Get SphinxSearch Data
-$query = $sphinx->prepare(qq|SHOW META|);
-$query->execute();
-my $arr_ref2 = $query->fetchall_arrayref({});
-my $hash_ref;
-for (@$arr_ref2) {
-    my $value = $_->{'Value'} =~ /^[\.\d]+$/ ? $_->{'Value'}+0 : $_->{'Value'};
-    $hash_ref->{$_->{'Variable_name'}} = $value;
-    }
-$data->{debug}->{'meta'} = $hash_ref;
-undef $arr_ref2;
-undef $hash_ref;
-
-$time = time;
-my $comments = ::getComments($dbh,[map {$_->{id}} @$arr_ref]);
-$data->{debug}->{dbtime} = time - $time;
-$time = time;
-for (@$arr_ref) {
-    next unless $comments->{$_->{id}}->{json};
-    my $json = decode_json($comments->{$_->{id}}->{json});
-    $json->{created_utc} = $json->{created_utc} + 0;   # force string to int for JSON
-    my $subreddit = $json->{subreddit};
-    delete $json->{subreddit};
-    my $submission_id = lc(cnv($_->{submission_id},10,36));
-    my $subreddit_id = lc(cnv($_->{subreddit_id},10,36));
-    my $comment_id = lc(cnv($_->{id},10,36));
-    my $score = $_->{score}+0;
-    my $count = $_->{c}+0;
-    my $body = $json->{body};
-    my $html = "";#markdown(decode_entities($body));
-    push(@{$data->{data}},{"top_comment" => $json , "subreddit" => $subreddit, "subreddit_id" => "t5_$subreddit_id", "link_id" => "t3_$submission_id", "count" => $count});
-    }
-$data->{debug}->{processing} = time - $time;
-$self->render(json => $data);
-$dbh->disconnect;
+#$query = $self->sphinx->prepare(qq|SHOW META|);
+#$query->execute();
+#my $arr_ref2 = $query->fetchall_arrayref({});
+#my $hash_ref;
+#for (@$arr_ref2) {
+#    my $value = $_->{'Value'} =~ /^[\.\d]+$/ ? $_->{'Value'}+0 : $_->{'Value'};
+#    $hash_ref->{$_->{'Variable_name'}} = $value;
+#    }
+#$data->{debug}->{'meta'} = $hash_ref;
+$self->render(json => $results);
+#$dbh->disconnect;
 }
 
 
